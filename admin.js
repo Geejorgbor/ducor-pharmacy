eee1b577528b042b9183e0f0914b488635464f2a
Geejorgbor <lucaspaye02@gmail.com>
2026-05-26 12:02:09 -0600

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2N6CcL0cGxBLfSdPANHJjjKuP5Rp0EIE",
+  authDomain: "ducor-pharmacy.firebaseapp.com",
+  projectId: "ducor-pharmacy",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login successful");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
